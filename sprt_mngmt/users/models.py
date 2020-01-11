e600bdcb78d451e56987609c52c1b94445b3a27d
vt <v@vt.com>
2020-01-12 00:34:27 +0530

    user models modified

--- sprt_mngmt/users/models.py
+++ sprt_mngmt/users/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-
-class Users(models.Model):
-    email = models.EmailField()
-    fname = models.CharField(max_length=255, required=True)
-    lname = models.CharField(max_length=255, required=True)
-    age = models.IntegerField(max_value=16, required=True)
