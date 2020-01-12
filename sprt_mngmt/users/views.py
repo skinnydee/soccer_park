b41757ef55473a2fd203e4e514de6a43ca9f0da7
vt <v@vt.com>
2020-01-12 12:14:17 +0530

    fixed registation form

--- sprt_mngmt/users/views.py
+++ sprt_mngmt/users/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
 
+
 def register(request):
     if request.method =='POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
