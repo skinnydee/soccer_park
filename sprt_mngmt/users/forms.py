b7153bf0a7b9902ce1f5ddab5816590475dd5826
vt <v@vt.com>
2020-01-14 00:17:14 +0530

    fixed registrations. User deteletion in process

--- sprt_mngmt/users/forms.py
+++ sprt_mngmt/users/forms.py
@@ -3,13 +3,30 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
     First_Name = forms.CharField(max_length=255, required=True)
     Last_Name = forms.CharField(max_length=255, required=True)
     age = forms.IntegerField(max_value=16, required=True)
-    
-
+    email = forms.EmailField()
+ 
 
     class Meta:
         model = User
-        fields = ('First_Name','Last_Name','age','username','email','password1','password2',)
+        fields = (
+                    'First_Name',
+                    'Last_Name',
+                    'age',
+                    'username',
+                    'email',
+                    'password1',
+                    'password2',
+                 )
+    
+    def save(self, commit = True):
+        user = super(UserRegisterForm, self).save(commit = False)
+        user.first_name = self.cleaned_data['First_Name']
+        user.last_name = self.cleaned_data['Last_Name']
+        user.age= self.cleaned_data['age']
+        user.emal = self.cleaned_data['email']
+
+        if commit:
+            user.save()
--- sprt_mngmt/users/models.py
+++ sprt_mngmt/users/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Proile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+def __str__(self):
+        return f'{self.user.username} Profile' 
