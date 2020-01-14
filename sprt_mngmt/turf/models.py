fc4a9cbe81f8ae280a4f3794c0f74cfbbfc4cc2d
vt <v@vt.com>
2020-01-14 23:15:45 +0530

    fixed user deleteion

--- sprt_mngmt/turf/models.py
+++ sprt_mngmt/turf/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class member(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(("Name"), max_length=150)
+    rank = models.IntegerField(("Rank"), max_length=16)
+    position = models.CharField(("Position"), max_length=150)
