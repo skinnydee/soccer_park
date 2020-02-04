93f298a111675b11119e9ef320b56ba43b339d44
Akash Roshan <akash.second1999@gmail.com>
2020-02-04 13:50:07 +0530

    fix display names and remove profile model fixes #1

--- sprt_mngmt/users/models.py
+++ sprt_mngmt/users/models.py
@@ -1,9 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    User = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profileusers")
-    iscoach = models.BooleanField(default=False)
-
-def __str__(self):
-        return f'{self.user.username} Profile' 
+# Create your models here.
