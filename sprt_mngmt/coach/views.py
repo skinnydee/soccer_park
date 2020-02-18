cb6c64e5813697059a373e00ccdec218d074a7f1
Soccerpark <dknew@protonmail.com>
2020-02-18 19:34:49 +0530

    db error

--- sprt_mngmt/coach/views.py
+++ sprt_mngmt/coach/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 
 def attendance(request):
     template_name = 'coach.attendance.html'
-    player = filter(User.objects.all)
+    player = filter(User.objects.na)
     return render(request,'coach/attendance.html', {'players' : player})
--- sprt_mngmt/sprt_mngmt/urls.py
+++ sprt_mngmt/sprt_mngmt/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as users_views
-from coach import urls
 
 
 urlpatterns = [
@@ -26,5 +25,5 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name = 'users/login.html'), name= 'login'),
     path('logout/',auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name= 'logout'),
     path('',include('turf.urls')),
-    path('coach',include('coach.urls'))
+
 ]
